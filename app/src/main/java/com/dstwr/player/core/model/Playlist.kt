6128af92c1e84844d05030f190bd79a4ad827bc0
mogahdbshar <mogahdbshar@gmail.com>
2026-09-22 02:16:27 +0300

    Resolve duplicate design-system and playlist declarations

--- app/src/main/java/com/dstwr/player/core/model/Playlist.kt
+++ app/src/main/java/com/dstwr/player/core/model/Playlist.kt
@@ -1,3 +1,7 @@
 package com.dstwr.player.core.model
-data class Playlist(val id:Long=0,val name:String,val url:String,val type:PlaylistType)
-enum class PlaylistType { M3U,Xtream,Stalker,LOCAL }
+data class Playlist(
+    val id: Long = 0,
+    val name: String,
+    val url: String,
+    val type: PlaylistType
+)
