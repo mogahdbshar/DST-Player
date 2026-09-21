f99a513e7d99b9728e860b5edaae1b72fc83ca3c
mogahdbshar <mogahdbshar@gmail.com>
2026-09-22 02:01:19 +0300

    Resolve complete build compilation dependencies

---
+++ app/src/main/java/com/dstwr/player/core/designsystem/Shapes.kt
@@ -0,0 +1,5 @@
+package com.dstwr.player.core.designsystem
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+val DSTShapes=Shapes(small=RoundedCornerShape(12.dp),medium=RoundedCornerShape(18.dp),large=RoundedCornerShape(24.dp))
