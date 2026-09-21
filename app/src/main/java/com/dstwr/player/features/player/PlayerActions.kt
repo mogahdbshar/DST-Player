1c8a4a44d0c96aedb06278fb99d2ca1ab1e26ae7
mogahdbshar <mogahdbshar@gmail.com>
2026-09-22 01:42:45 +0300

    Complete DST Player integration and playback foundation

---
+++ app/src/main/java/com/dstwr/player/features/player/PlayerActions.kt
@@ -0,0 +1,2 @@
+package com.dstwr.player.features.player
+data class PlayerActions(val subtitles:Boolean=false,val audioTracks:Boolean=false,val quality:Boolean=false,val pip:Boolean=false,val cast:Boolean=false,val locked:Boolean=false)
