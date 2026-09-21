769c9d0678c0966f38952a15d2bb8843215242a1
mogahdbshar <mogahdbshar@gmail.com>
2026-09-22 01:38:37 +0300

    Complete DST Player phase 1 core architecture

---
+++ app/src/main/java/com/dstwr/player/core/utils/ResultState.kt
@@ -0,0 +1,2 @@
+package com.dstwr.player.core.utils
+sealed interface ResultState<out T>{data object Loading:ResultState<Nothing>;data class Success<T>(val data:T):ResultState<T>;data class Error(val message:String):ResultState<Nothing>}
