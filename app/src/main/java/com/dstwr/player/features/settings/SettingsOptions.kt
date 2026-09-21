8e965b4bce7db1262275124a09a865c00857e039
mogahdbshar <mogahdbshar@gmail.com>
2026-09-22 01:42:59 +0300

    Complete DST Player integration and playback foundation

---
+++ app/src/main/java/com/dstwr/player/features/settings/SettingsOptions.kt
@@ -0,0 +1,2 @@
+package com.dstwr.player.features.settings
+data class SettingsOptions(val language:String="ar",val autoplay:Boolean=true,val subtitles:Boolean=true,val rememberPosition:Boolean=true,val darkMode:Boolean=true)
